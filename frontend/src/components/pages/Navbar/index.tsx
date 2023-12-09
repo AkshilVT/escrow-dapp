b3517fa4d839fa20579dc5bf9f62192898009fff
Office <twinklevpopat20@gmail.com>
2023-12-10 04:42:37 +0530

    adding assigned and mytask pages 3

--- frontend/src/components/pages/Navbar/index.tsx
+++ frontend/src/components/pages/Navbar/index.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav style={{ margin: '25px' }} className="bg-blue-700 rounded">
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-between">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <button type="button"
+          onClick={toggleMobileMenu}
+           className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu"
+           aria-expanded={isMobileMenuOpen}>
             <span className="absolute -inset-0.5"></span>
             <span className="sr-only">Open main menu</span>
            
@@ -20,9 +30,9 @@ export default function Navbar() {
           </button>
         </div>
         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="flex flex-shrink-0 items-center">
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>
-          </div>
+          {/* <div className="flex flex-shrink-0 items-center">
+          //Add logo here
+          </div> */}
           <div className="hidden sm:ml-6 sm:block">
             <div className="flex space-x-4">
             <Link href="/Explore" passHref>
@@ -44,13 +54,7 @@ export default function Navbar() {
           </div>
         </div>
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span className="absolute -inset-1.5"></span>
-            <span className="sr-only">View notifications</span>
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-            </svg>
-          </button>
+         
   
           <div className="relative ml-3">
             <div>
@@ -68,12 +72,23 @@ export default function Navbar() {
       </div>
     </div>
   
-    <div className="sm:hidden" id="mobile-menu">
+    <div className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
       <div className="space-y-1 px-2 pb-3 pt-2">
-        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+      <Link href="/Explore" passHref>
+                    <div className="cursor-pointer text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium" aria-current="page">
+                      Explore
+                    </div>
+                  </Link>
+                  <Link href="/Assigned" passHref>
+                    <div className="cursor-pointer text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">
+                      Assigned
+                    </div>
+                  </Link>
+                  <Link href="/Mytasks" passHref>
+                    <div className="cursor-pointer text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">
+                      MyTasks
+                    </div>
+                  </Link>
       </div>
     </div>
   </nav>
