95e5bc488e03355a7297f052f2a1a67480a34e5a
Office <twinklevpopat20@gmail.com>
2023-12-10 04:26:53 +0530

    adding assigned and mytask pages 2

--- frontend/src/components/pages/Navbar/index.tsx
+++ frontend/src/components/pages/Navbar/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav style={{ margin: '25px' }} className="bg-blue-700 rounded-full">
+    <nav style={{ margin: '25px' }} className="bg-blue-700 rounded">
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-between">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
